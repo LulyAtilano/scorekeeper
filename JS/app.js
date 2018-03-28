5d3ac44797af8142fbeca751412079a341055f85
latilanoh@gmail.com <latilanoh@gmail.com>
2018-03-28 11:16:50 -0600

    Se agregan 3 funciones en el Js para logra la funcionalidad en el ejercicio a resolver

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,38 @@
+// Objeto SCORES va a llevar el registro de los jugadores y sus puntos.
+var scores = {};
+console.log(scores);
 
+// crear función para generar los keys del objeto score
+function createPlayer(object,name) {
+  object[name.toLowerCase()] = 0;
+  console.log(object);
+}; // function
+
+// Le estoy asignando los value a las keys que están en el objeto SCORES
+function addPoints(name,points) {
+  scores[name.toLowerCase()] += points;
+  console.log(scores);
+}; // function
+
+// función para imprimir todos los puntos de los jugadores
+function printAllPoints() {
+  var arrayPlayers = Object.keys(scores); // tengo las keys del objeto
+  var resultados = "";
+  for (var k = 0; k < arrayPlayers.length; k++) {
+    resultados += arrayPlayers[k] + ": " + scores[arrayPlayers[k]] + ", ";
+    if ( k == arrayPlayers.length -1) { // se verifica que estemos en el último index para quitar la coma que separa los elementos
+      resultados += arrayPlayers[k] + ": " + scores[arrayPlayers[k]];
+    } else { // si NO es el ultimo index se deja la coma de sepación.
+      resultados += arrayPlayers[k] + ": " + scores[arrayPlayers[k]] + ", ";
+       } // if
+    } //for
+    return resultados;
+  }; // function
+
+
+createPlayer(scores, "Ana");
+addPoints("Ana", 20);
+addPoints("ana", 40);
+createPlayer(scores, "Silvana");
+addPoints("silvana", 40)
+// console.log(printAllPoints());
--- README.md
+++ README.md
@@ -25,4 +25,4 @@ En esta parte se describe la funcionalidad del proyecto descrito en pasos.
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-![Diagrama de flujo del problema SCOREKEEPER](assets/img/diagrama-flujo-scorekeeper.png)
+![Diagrama de flujo del problema SCOREKEEPER](assets/images/diagrama-flujo-scorekeeper.png)
